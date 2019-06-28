2eeb3ae5f3050c6f8a5b349717ee0a8fc52b27ad
John Louis Del Rosario <john2x@gmail.com>
2019-06-28 19:42:30 +1200

    Add more keywords

--- jenkinsfile-mode.el
+++ jenkinsfile-mode.el
@@ -28,9 +28,35 @@
 
 
 (setq jenkinsfile-mode-font-lock-keywords
-      '(("pipeline" . font-lock-preprocessor-face)
+      `(("pipeline" . font-lock-preprocessor-face)
         ("agent\\|post\\|stages\\|steps" . font-lock-builtin-face)
-        ("environment\\|options\\|parameters\\|triggers\\|stage\\|tools\\|input\\|when\\|parallel\\|script" . font-lock-constant-face)))
+        (,(regexp-opt '("environment" "options" "parameters"
+                        "triggers" "stage" "tools" "input"
+                        "when" "parallel" "script") 'symbols)
+         . font-lock-constant-face)
+        (,(regexp-opt '("always" "changed" "fixed"
+                        "regression" "aborted" "failure" "success"
+                        "unstable" "unsuccessful" "cleanup") 'symbols)
+         . font-lock-keyword-face)
+        (,(regexp-opt '("buildDiscarder" "checkoutToSubdirectory"
+                        "disableConcurrentBuilds" "disableResume"
+                        "newContainerPerStage" "overrideIndexTriggers"
+                        "preserveStashes" "quietPeriod" "retry"
+                        "skipDefaultCheckout" "skipStagesAfterUnstable"
+                        "timeout" "timestamps" "parallelsAlwaysFailFast") 'symbols)
+         . font-lock-keyword-face)
+        (,(regexp-opt '("string" "text" "booleanParam" "choice" "file"
+                        "password") 'symbols)
+         . font-lock-keyword-face)
+        (,(regexp-opt '("cron" "pollSCM" "upstream") 'symbols)
+         . font-lock-keyword-face)
+        (,(regexp-opt '("id" "ok" "subitter" "submitterParameter" "parameters") 'symbols)
+         . font-lock-keyword-face)
+        (,(regexp-opt '("branch" "buildingTag" "changelog" "changeset" "changeRequest"
+                        "environment" "equals" "expression" "tag" "not" "allOf"
+                        "anyOf" "triggeredBy") 'symbols)
+         . font-lock-keyword-face)
+        ))
 
 (setq jenkinsfile-mode-font-lock-defaults
       (append groovy-font-lock-keywords jenkinsfile-mode-font-lock-keywords))
